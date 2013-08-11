7b36732e7fd76409f2233441f651016b233ca817
Blauwbek <blauwbek@gmail.com>
2013-08-11 20:50:12 +0200

    Better output, added job result check (untested)

--- pySabRename_win_dev.py
+++ pySabRename_win_dev.py
@@ -24,12 +24,16 @@ import os
 final_dir = sys.argv[1]
 #name to rename the biggest file to
 job_name = sys.argv[3]
+#job result
+job_res = sys.argv[7]
 
 #non-sab things
 #largest file so far and size, for use in find(dir)
 r_file = ('', -1)
 
+#file extensions, edit at will
 extlist = ('.mkv', '.avi', '.mp4', '.3gp', '.divx', '.flv', '.mpg', '.m4v', '.mov', '.mpeg', '.swf', '.wmv', '.iso')
+#subtitle extensions, also open for additions
 sublist = ('.idx', '.sub', '.srt')
 
 
@@ -51,6 +55,11 @@ print '| To address:                              |'
 print '| 14QFusmzBTAo9FhH7x7puHNkdCps8vamVV       |'
 print '+------------------------------------------+'
 
+if int(job_res) != 0:
+	print 'To prevent bad stuff from happening we do not run this script on jobs that are not succesfully completed (par/rar fail)'
+	print 'If you want to run this anyway try this in commandline:'
+	print sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7]
+
 def find(dir):
 	print "Searching dir: ", dir
 	global r_file
@@ -63,42 +72,34 @@ def find(dir):
 			if itemsize > r_file[1]:
 				r_file = (item, itemsize)
 
-print "\n+Renaming process+"
-
 find(final_dir)
+print "\n+Renaming process+"
 if r_file[1] != -1:
 	filename = os.path.basename(r_file[0])
-	print 'Found: ', filename
+	print 'Found:', filename
 
 old_name = r_file[0]
-print 'Old name: ', old_name
+print 'Old name:', old_name
 
 filename, ext = os.path.splitext(r_file[0])
 print 'Found extension! ({})'.format(ext)
 
 if any(ext == val for val in extlist):
-	print 'Extention supported! ', ext
+	print 'Extention supported!', ext
 	new_file = os.path.join(final_dir, job_name+ext)
 	print 'New name:', new_file
 	os.rename(old_name, new_file)
 else:
-	print 'This file has an extension that is not supported to prevent wrong renames like multi-file movies (dvds etc.) ', ext
+	print 'This file has an extension that is not supported to prevent wrong renames like multi-file movies (dvds etc.):', ext
 
 for s_ext in sublist:
 	if os.path.isfile(filename+s_ext):
 		os.rename(filename+s_ext, os.path.join(final_dir, job_name+s_ext))
-		print 'We found and renamed a subtitle file with the extension ', s_ext
-
-for item in os.listdir(final_dir):
-	if os.path.isdir(item):
-		if not os.listdir(item):
-			os.rmdir(item)
-			print 'Removed empty folder', item
-
-print "\n+Cleaning process+"
+		print 'We found and renamed a subtitle file with the extension', s_ext
 
 def cleanup(top):
 	global final_dir, job_name, ext, sublist
+	print "\n+Cleaning process+"
 	if top == '/' or top == '\\':
 		print 'nope.jpg'
 	else:
@@ -106,13 +107,13 @@ def cleanup(top):
 			for name in files:
 				if not any(os.path.splitext(name)[1] == s_ext for s_ext in sublist) and not os.path.join(root, name) == os.path.join(final_dir, job_name+ext) and not os.path.splitext(name)[1] == '.nfo':
 					os.remove(os.path.join(root, name))
-					print 'Removed: ', os.path.join(root, name)
+					print 'Removed:', os.path.join(root, name)
 			for name in dirs:
 				try:
 					os.rmdir(os.path.join(root, name))
-					print 'Removed dir: ', os.path.join(root, name)
+					print 'Removed dir:', os.path.join(root, name)
 				except:
-					print 'Unable to delete dir: ', os.path.join(root, name)
+					print 'Unable to delete dir:', os.path.join(root, name)
 
 cleanup(final_dir)
 
