a6b57a437d876932191571d1823b65981e7f761e
Blauwbek <blauwbek@gmail.com>
2014-01-08 00:00:22 +0100

    Dev is now default. Unix == Win from now on.

--- pySabRename_syno.py
+++ pySabRename_syno.py
@@ -17,16 +17,30 @@
 
 import sys
 import os
+import ConfigParser
+
+#config stuff
+config = ConfigParser.RawConfigParser()
+config.readfp(open(os.path.join(os.path.dirname(sys.argv[0]), 'pySR.cfg')))
 
 #sab things
 #dir the files are we want to use
 final_dir = sys.argv[1]
 #name to rename the biggest file to
 job_name = sys.argv[3]
+#user defined cat
+job_cat = sys.argv[5]
+#job result
+job_res = sys.argv[7]
 
 #non-sab things
 #largest file so far and size, for use in find(dir)
-r_file = ("", -1)
+r_file = ('', -1)
+
+#file extensions, edit at will
+extlist = config.get('Static', 'vidext').split(',')
+#subtitle extensions, also open for additions
+sublist = config.get('Static', 'subext').split(',')
 
 
 print 'pySabRename\n'
@@ -47,11 +61,16 @@ print '| To address:                              |'
 print '| 14QFusmzBTAo9FhH7x7puHNkdCps8vamVV       |'
 print '+------------------------------------------+'
 
+if int(job_res) != 0:
+	print 'To prevent bad stuff from happening we do not run this script on jobs that are not succesfully completed (par/rar fail)'
+	print 'If you want to run this anyway try this in commandline:'
+	print sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7]
+
 def find(dir):
 	print "Searching dir: ", dir
 	global r_file
 	for item in os.listdir(dir):
-		item = dir + "/" + item
+		item = os.path.join(dir, item)
 		if os.path.isdir(item):
 			find(item)
 		else:
@@ -60,17 +79,66 @@ def find(dir):
 				r_file = (item, itemsize)
 
 find(final_dir)
+print "\n+Renaming process+"
 if r_file[1] != -1:
-	print "Found: ", os.path.basename(r_file[0])
+	filename = os.path.basename(r_file[0])
+	print 'Found:', filename
 
 old_name = r_file[0]
-print "Old name: ", old_name
+print 'Old name:', old_name
 
-ext = os.path.splitext(r_file[0])[1]
-print "Found extension! ({})".format(ext)
+filename, ext = os.path.splitext(r_file[0])
+print 'Found extension! ({})'.format(ext)
 
-new_file = final_dir+ "/" + job_name + ext
-print "New name: ", new_file
-os.rename(old_name, new_file)
+if any(ext == val for val in extlist):
+	print 'Extention supported!', ext
+	new_file = os.path.join(final_dir, job_name+ext)
+	print 'New name:', new_file
+	os.rename(old_name, new_file)
+else:
+	print 'This file has an extension that is not supported to prevent wrong renames like multi-file movies (dvds etc.):', ext
+
+for s_ext in sublist:
+	if os.path.isfile(filename+s_ext):
+		os.rename(filename+s_ext, os.path.join(final_dir, job_name+s_ext))
+		print 'We found and renamed a subtitle file with the extension', s_ext
+
+def cleanup(top):
+	global final_dir, job_name, ext, sublist
+	print "\n+Cleaning process+"
+	if top == '/' or top == '\\':
+		print 'nope.jpg'
+	else:
+		for root, dirs, files in os.walk(top, topdown=False):
+			for name in files:
+				if not any(os.path.splitext(name)[1] == s_ext for s_ext in sublist) and not os.path.join(root, name) == os.path.join(final_dir, job_name+ext) and not os.path.splitext(name)[1] == '.nfo':
+					os.remove(os.path.join(root, name))
+					print 'Removed:', os.path.join(root, name)
+			for name in dirs:
+				try:
+					os.rmdir(os.path.join(root, name))
+					print 'Removed dir:', os.path.join(root, name)
+				except:
+					print 'Unable to delete dir:', os.path.join(root, name)
+
+if config.get('sabnzbd', 'moviecat')==job_cat:
+	if config.getboolean('movies', 'cleanup'):
+		cleanup(final_dir)
+
+if config.get('sabnzbd', 'tvcat')==job_cat:
+	if config.getboolean('tv', 'cleanup'):
+		cleanup(final_dir)
+
+	if config.getboolean('tv', 'sickbeard'):
+		print '\n+Calling Sickbeard+'
+		try:
+			import autoProcessTV
+			
+			if autoProcessTV.processEpisode.func_code.co_argcount == 2:
+				autoProcessTV.processEpisode(final_dir, sys.argv[2])
+			elif autoProcessTV.processEpisode.func_code.co_argcount == 3:
+				autoProcessTV.processEpisode(final_dir, sys.argv[2], job_res)
+		except:
+			print 'Could not run sickbeard, is autoProcessTV in the same folder as this script?'
 
 sys.exit(0)
